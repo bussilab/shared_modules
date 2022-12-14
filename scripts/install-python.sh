8afdc6312641c9537b9bcd46ac1c4cee357883d7
Giovanni Bussi <giovanni.bussi@gmail.com>
2022-12-15 00:13:38 +0100

    python 3.9.15

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -1,19 +1,40 @@
+umask 022
 export TMPDIR="/scratch/$USER/tmp"
+
+module purge
 
 TMP="$(mktemp -d)"
 
 BASEDIR=$(cd .. && pwd)
 
-version=3.9.14
-shortversion=3.9
+versions="3.9.14 3.9.15"
+versions=3.9.15
+
+for version in $versions
+do
+
+( # run in separate shell
+
+shortversion=${version%.*}
 
 prefix=$BASEDIR/install/python-$version
 moduledirbase=$BASEDIR/modules/python
 moduledir=$moduledirbase/$shortversion
 
-#install modulefile
-mkdir -p $moduledir
-touch $moduledirbase/.version
+# install python
+true &&
+cd $TMP &&
+curl -LO https://www.python.org/ftp/python/$version/Python-$version.tgz &&
+tar xzf Python-$version.tgz &&
+cd Python-$version &&
+./configure --prefix=$prefix &&
+make -j 12 &&
+make install &&
+chmod -R a-w $prefix &&
+chmod -R a+rX $prefix &&
+rm -fr $TMP &&
+mkdir -p $moduledir &&
+touch $moduledirbase/.version &&
 cat > $moduledir/$version << EOF
 #%Module1.0##############################################
 
@@ -30,16 +51,6 @@ prepend-path    PKG_CONFIG_PATH $prefix/lib/pkgconfig
 module use $BASEDIR/add-modules/py$shortversion
 EOF
 
-# install python
-true &&
-cd $TMP &&
-curl -LO https://www.python.org/ftp/python/$version/Python-$version.tgz &&
-tar xzf Python-$version.tgz &&
-cd Python-$version &&
-./configure --prefix=$prefix &&
-make -j 12 &&
-make install &&
-chmod -R a-w $prefix &&
-chmod -R a+rX $prefix &&
-rm -fr $TMP
+)
 
+done
