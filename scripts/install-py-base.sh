f69d78952f312c6f3d79bbb71694bc87578843ef
Giovanni Bussi <giovanni.bussi@gmail.com>
2023-04-04 14:53:06 +0200

    updated py-base

--- scripts/install-py-base.sh
+++ scripts/install-py-base.sh
@@ -1,19 +1,26 @@
+umask 022
 BASEDIR=$(cd .. && pwd)
 
 pyversion=3.9
-version=2022.11.29
 module=py-base
+version=2023.04.04
+#stackon_version=2022.11.29
 
 prefix=$BASEDIR/install/py$pyversion-$module-$version
 moduledir=$BASEDIR/add-modules/py$pyversion/$module
 
 packages="
-argcomplete networkx numba numpy scipy slackclient slack_sdk pyyaml typing_extensions
+argcomplete networkx numba numpy<1.24 scipy slackclient slack_sdk pyyaml typing_extensions
 pandas scikit-learn matplotlib jupyter jupyterlab
 cython
 nglview panedr plumed mdtraj barnaba MDAnalysis
 pytest pytest-cov pyflakes pylint flake8 pdoc3 mypy nbconvert twine nose
 toml
+acpype
+asciinema pygments
+ipython rdkit biopython pydca PlumedToHTML
+sympy regex openpyxl multiprocess
+tqdm
 "
 
 requirements=$module-$version-requirements.txt
@@ -27,26 +34,33 @@ test -f $requirements && {
   packages=$(cat $requirements)
 }
 
+module purge
+module use $BASEDIR/modules
+module load python/$pyversion
+test -n "$stackon_version" && module load $module/$stackon_version
+
+export PATH=$prefix/bin:$PATH
+export PYTHONPATH=$prefix/lib/python$pyversion/site-packages:$PYTHONPATH
+export LD_LIBRARY_PATH=$prefix/lib:$prefix/lib64:$LD_LIBRARY_PATH
+
+# install packages # might need --use-deprecated=legacy-resolver
+true &&
+mkdir -p $prefix &&
+PYTHONUSERBASE=$prefix pip3 install --user -U $packages &&
+pip3 freeze > $requirements &&
+chmod -R a-w $prefix &&
+chmod -R a+rX $prefix 
+
 #install modulefile
 mkdir -p $moduledir
 
-cat > $moduledir/$version << EOF
+cat > $moduledir/.$version << EOF
 #%Module1.0##############################################
 
 module-whatis   "Python modules $version"
+$(test -n "$stackon_version" && grep prepend-path $moduledir/$stackon_version)
 prepend-path    PATH            $prefix/bin
 prepend-path    PYTHONPATH      $prefix/lib/python$pyversion/site-packages
 prepend-path    LD_LIBRARY_PATH $prefix/lib:$prefix/lib64
 EOF
-
-module use $BASEDIR/modules
-module load python/$pyversion
-module load $module/$version
-
-# install packages
-true &&
-pip3 install --ignore-installed --prefix=$prefix $packages &&
-pip3 freeze > $requirements &&
-chmod -R a-w $prefix &&
-chmod -R a+rX $prefix
 
